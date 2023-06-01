e6b84a91b9a8546c99bcb72df6981e6573c5cebb
JaxonBryant <125932123+JaxonBryant@users.noreply.github.com>
2023-06-01 02:52:07 +0000

    Working yes/no function
    The last one didn't detect if the answer was true or false, and this one is able to affect different variables instead of the single universal one.

--- main.py
+++ main.py
@@ -1,1 +1,2 @@
 import yes_no
+
--- yes_no.py
+++ yes_no.py
@@ -1,11 +1,23 @@
-def yes_no(question):
 
-  response = input(question).lower().strip()
-  if response == "yes" or response == "y":
-    print("Program continues ")
-    return response
-  elif response == "no" or response == "n":
-    print("Program continues ")
-    return response
-  else:
-    print("Please input yes or no ")
+def yes_no(question, answer_variable):
+  while True:
+    user_input = input(question).lower().strip()
+    if user_input == "yes" or user_input == "y":
+      answer_variable = True
+      print("You entered yes ")
+      break
+    elif user_input == "no" or user_input == "n":
+      answer_variable = False
+      print("You entered no ")
+      break
+    else:
+      print("Invalid input. Please enter 'yes' or 'no'. ")
+      return answer_variable
+
+#setting up variables
+used_program = False
+
+#Loop for testing
+valid = False
+while not valid:
+  result = yes_no("Have you used this program before? ", used_program)
